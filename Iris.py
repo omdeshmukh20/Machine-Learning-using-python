7f381f60767e43acc79f2f633c24b0eb83ce4401
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-17 19:17:50 +0530

    Load iris dataset

---
+++ Iris.py
@@ -0,0 +1,17 @@
+#Discription: Load iris dataset
+#Date: 17/07/21
+#Author : Om Deshmukh
+from sklearn.datasets import load_iris
+
+def main():
+	datasets=load_iris()
+
+	print("Features of dataset")
+	print(datasets.feature_names)
+
+	print("Target_names of dataset")
+	print(datasets.target_names)
+
+
+if __name__ == '__main__':
+	main()
