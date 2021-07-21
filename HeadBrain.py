c52e1ec85cc3c4967be03eb46b25c853dfdc6341
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-21 16:43:04 +0530

    Studing head brain dataset

---
+++ HeadBrain.py
@@ -0,0 +1,34 @@
+#Discription: Studing head brain dataset
+#Date: 21/07/21
+#Author : Om Deshmukh
+
+import pandas as pd 
+import numpy as pd
+
+def MeanData(arr):
+	size=len(arr)
+	sum=0
+
+	for i in range(size):
+		sum
+
+
+def HeadBrain(Name):
+	dataset=pd.read_csv(Name)
+	print("Size of our dataset is",dataset.shape)
+
+	X=datset["Head Size(cm^3"].values
+	Y=dataset["Brain Weight(grams"].values
+
+	print("Length of X",len(X))
+	print("Length of Y",len(y))
+
+
+def main():
+	print("Enter file name of dataset")
+	name=input()
+
+	HeadBrain(name)
+
+if __name__ == '__main__':
+		main()	
