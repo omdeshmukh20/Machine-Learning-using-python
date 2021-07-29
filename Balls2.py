6a102da54b30a99087fd51f849e33fb6a905e46c
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-29 10:08:25 +0530

    Supervised ML using Dicision tree classifier

--- Balls2.py
+++ Balls2.py
@@ -2,18 +2,42 @@
 #Date: 17/07/21
 #Author : Om Deshmukh
 
+
+#'''
+
+#Classifier        :   Decision Tree
+#Dataset           :   Balls Dataset
+#Features          :   Weight & Surface Type
+#Labels            :   Tennis And Cricket 
+#Training Dataset  :   15 Entries
+#Testing Dataset   :   1 Entry
+#Author            :   Prasad Dangare
+#Function Name     :   Balls_Dataset
+
+#'''
+
+
 from sklearn import tree
+
 # Rough 1 # Smooth 0 # Tennis 1 # Cricket 2
 
 def MarvellousML(weight,surface):
+    
     # Step 1 & 2
+    
     Features = [[35,1],[47,1],[90,0],[48,1],[90,0],[35,1],[92,0],[35,1],[35,1],[35,1],[96,0],[43,1],[110,0],[35,1],[95,0]]
     Labele = [1,1,2,1,2,1,2,1,1,1,2,1,2,1,2]
+    
     # Step 3
+    
     dobj = tree.DecisionTreeClassifier()
+    
     #Step 4
+    
     dobj.fit(Features,Labele)
+    
     #Step 5
+    
     result = dobj.predict([[weight,surface]])
     if result == 1:
         print("Your object looks like Tennis ball")
